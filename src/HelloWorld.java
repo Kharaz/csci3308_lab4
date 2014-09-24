6bf5a7f8e42815c0bf834a58dc16627b7cd67442
sruppgreene <siru4581@colorado.edu>
2014-09-23 23:04:27 -0600

    second commit i did stuff yay

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello CSCI3308!");
+		System.out.println("lookit me i have changed the code.");
 	}
 
 }
